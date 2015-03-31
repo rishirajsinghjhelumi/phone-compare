0e4ccdbfff0d9819469843b5c5df22ffaae3c943
Rishi Raj Singh Jhelumi <rishiakhnoor@gmail.com>
2015-03-31 19:59:08 +0530

    add user methods

--- app/__init__.py
+++ app/__init__.py
@@ -1,50 +1,28 @@
-import os
-import sys
-
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
+
+from util import install_secret_key
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
-########################
-# Configure Secret Key #
-########################
-def install_secret_key(app, filename='secret_key'):
-    """Configure the SECRET_KEY from a file
-    in the instance directory.
-
-    If the file does not exist, print instructions
-    to create it from a shell with a random key,
-    then exit.
-    """
-    filename = os.path.join(app.instance_path, filename)
-
-    try:
-        app.config['SECRET_KEY'] = open(filename, 'rb').read()
-    except IOError:
-        print('Error: No secret key. Create it with:')
-        full_path = os.path.dirname(filename)
-        if not os.path.isdir(full_path):
-            print('mkdir -p {filename}'.format(filename=full_path))
-        print('head -c 24 /dev/urandom > {filename}'.format(filename=filename))
-        sys.exit(1)
-
 if not app.config['DEBUG']:
     install_secret_key(app)
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+    # return render_template('404.html'), 404
+    return jsonify(status = "Page Not Found"), 404
+
+@app.errorhandler(403)
+def forbidden_page(error):
+    return  jsonify(status = "Forbidden Page"), 403
+
+@app.errorhandler(500)
+def server_error_page(error):
+    return jsonify(status = "Server Error"), 500
 
 #from app.users.views import mod as usersModule
 #app.register_blueprint(usersModule)
-
-# Later on you'll import the other blueprints the same way:
-#from app.comments.views import mod as commentsModule
-#from app.posts.views import mod as postsModule
-#app.register_blueprint(commentsModule)
-#app.register_blueprint(postsModule)
-
