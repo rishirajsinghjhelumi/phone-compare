cd036b510dfa23878c2f756c1b848d78538571be
Rishi Raj Singh Jhelumi <rishiakhnoor@gmail.com>
2015-04-05 23:10:15 +0530

    add phone view

--- app/phones/views.py
+++ app/phones/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
 from flask import jsonify
+from bson.objectid import ObjectId
 
 from bson import json_util
 import json
@@ -12,5 +13,13 @@ mod = Blueprint('phones', __name__, url_prefix='/phone')
 @mod.route('/brand/<brandName>', methods=['GET', 'POST'])
 @jsonResponse
 def brandModels(brandName):
+
 	phones = mongo.phones.find({"Brand" : brandName}, {"Model Name" : 1})
 	return [phone for phone in phones]
+
+@mod.route('/detail/<phoneID>', methods=['GET', 'POST'])
+@jsonResponse
+def phoneDetail(phoneID):
+
+	phone = mongo.phones.find_one({"_id" : ObjectId(phoneID)})
+	return phone
