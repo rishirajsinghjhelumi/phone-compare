826f1717e2964c62ae850cee4a95202972730be8
Rishi Raj Singh Jhelumi <rishiakhnoor@gmail.com>
2015-04-05 21:43:16 +0530

    add json resposne decorator

--- app/decorators.py
+++ app/decorators.py
@@ -1,7 +1,14 @@
 import json
 from bson import json_util
+from flask import Response
 
 def mongoJsonify(func):
 	def JSON(*args, **kwargs):
 		return json.dumps(func(*args, **kwargs), indent = 4, default = json_util.default)
+	return JSON
+
+def jsonResponse(func):
+	def JSON(*args, **kwargs):
+		text = json.dumps(func(*args, **kwargs), indent = 4, default = json_util.default)
+		return Response(response = text, status = 200, mimetype = 'application/json')
 	return JSON
