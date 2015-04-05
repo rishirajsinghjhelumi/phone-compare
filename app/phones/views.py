ecc0a36c26b7b59b5ad63b207e286957286fdf19
Rishi Raj Singh Jhelumi <rishiakhnoor@gmail.com>
2015-04-05 20:07:18 +0530

    add phone views

--- app/phones/views.py
+++ app/phones/views.py
@@ -1,10 +1,15 @@
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
 from flask import jsonify
 
+from bson import json_util
+import json
+
 from app import mongo
+from app.decorators import mongoJsonify
 
 mod = Blueprint('phones', __name__, url_prefix='/phone')
 
 @mod.route('/all/', methods=['GET', 'POST'])
-def login():
-	pass
+@mongoJsonify
+def all():
+	return mongo.phones.find_one()
