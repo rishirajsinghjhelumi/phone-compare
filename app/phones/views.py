edf2bc4e3303b0fc30c2b1bed5e21b55ecf23209
Rishi Raj Singh Jhelumi <rishiakhnoor@gmail.com>
2015-04-06 05:42:23 +0530

    add phone review, sentiments

--- app/phones/views.py
+++ app/phones/views.py
@@ -11,25 +11,50 @@ from app.util import getArgAsList
 
 mod = Blueprint('phones', __name__, url_prefix='/phone')
 
+def getPhoneInfo(phoneID):
+	
+	phone = mongo.phones.find_one({
+		"_id" : ObjectId(phoneID)
+	})
+	
+	phone["Reviews"] = mongo.reviews.find_one({
+		"Brand" : phone["Brand"],
+		"Model Name" : phone["Brand"] + " " + phone["Model Name"]
+	})["Reviews"]
+	
+	phone["Sentiments"] = mongo.sentiments.find_one({
+		"Brand" : phone["Brand"],
+		"Model Name" : phone["Brand"] + " " + phone["Model Name"]
+	})["Sentiments"]
+
+	return phone
+
 @mod.route('/brand/<brandName>', methods=['GET'])
 @jsonResponse
 def brandModels(brandName):
 
-	phones = mongo.phones.find({"Brand" : brandName}, {"Model Name" : 1})
+	phones = mongo.phones.find({
+		"Brand" : brandName
+		}, {
+		"Model Name" : 1
+	})
 	return [phone for phone in phones]
 
 @mod.route('/detail/<phoneID>', methods=['GET'])
 @jsonResponse
 def phoneDetail(phoneID):
 
-	phone = mongo.phones.find_one({"_id" : ObjectId(phoneID)})
-	return phone
+	return getPhoneInfo(phoneID)
 
 @mod.route('/details', methods=['GET'])
 @jsonResponse
 def phones():
 
 	phoneIds = getArgAsList(request, 'ids')
-	phoneIds = map(ObjectId, phoneIds)
-	phones = mongo.phones.find({"_id" : { "$in" : phoneIds}})
-	return [phone for phone in phones]
+	# phoneIds = map(ObjectId, phoneIds)
+	# phones = mongo.phones.find({
+	# 	"_id" : { 
+	# 		"$in" : phoneIds
+	# 	}
+	# })
+	return [getPhoneInfo(phone) for phone in phoneIds]
