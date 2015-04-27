ddc07d198400d744c3bd079320fe6df5d8e05f2f
Ubuntu <ubuntu@ip-172-31-36-50.ec2.internal>
2015-04-27 11:42:18 +0000

    Adding the ECommerce prices in the phone details blob

--- app/phones/views.py
+++ app/phones/views.py
@@ -6,26 +6,61 @@ from bson import json_util
 import json
 
 from app import mongo
+from app import app
 from app.decorators import mongoJsonify, jsonResponse
+import logging
 from app.util import getArgAsList
+from logging.handlers import RotatingFileHandler
 
 mod = Blueprint('phones', __name__, url_prefix='/phone')
 
+formatter = logging.Formatter(
+        "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+handler = RotatingFileHandler("/var/www/html/wordpress/phone-compare/app/phones/logs/application.log", maxBytes=10000000, backupCount=5)
+#handler.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.DEBUG)
+
 def getPhoneInfo(phoneID):
-	
+	app.logger.info(phoneID)
 	phone = mongo.phones.find_one({
 		"_id" : ObjectId(phoneID)
 	})
-	
-	phone["Reviews"] = mongo.reviews.find_one({
-		"Brand" : phone["Brand"],
-		"Model Name" : phone["Brand"] + " " + phone["Model Name"]
-	})["Reviews"]
-	
-	phone["Sentiments"] = mongo.sentiments.find_one({
-		"Brand" : phone["Brand"],
-		"Model Name" : phone["Brand"] + " " + phone["Model Name"]
-	})["Sentiments"]
+        app.logger.info(phone)
+	try:
+		phone["Reviews"] = mongo.reviews.find_one({
+			"Brand" : phone["Brand"],
+			"Model Name" : phone["Brand"] + " " + phone["Model Name"]
+		})["Reviews"]
+	except:
+		 app.logger.error("Review data not found for phone with ID: ")
+	try:
+		prices = mongo.prices.find({
+                        "Model Name" : phone["Brand"] + " " + phone["Model Name"]
+                })
+		eComList = []
+		app.logger.info(prices)
+		for price in prices:
+			eCom = {}
+			eCom["website"] = price["ECommerceName"]
+			eCom["price"] = price["ECommercePrice"]
+			eCom["stock status"] = price["ECommerceStatus"]
+			eComList.extend([eCom])
+		phone["Prices"] = eComList
+		app.loger.info(phone["Prices"])
+        except:
+                app.logger.error("Price data not found for phone with ID: ")
+                app.logger.error(phoneID)
+
+	try:
+		phone["Sentiments"] = mongo.sentiments.find_one({
+			"Brand" : phone["Brand"],
+			"Model Name" : phone["Brand"] + " " + phone["Model Name"]
+		})["Sentiments"]
+	except:
+		app.logger.error("Sentiment data not found for phone with ID: ")
+                app.logger.error(phoneID)
 
 	return phone
 
