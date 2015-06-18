d4fc9dd30975e4060f6e05be15afab63cb4422e5
Abhishek <dude.abhi.chat@gmail.com>
2015-06-19 01:50:46 +0530

    implementing best Reco phones

--- scripts/calculateBestWhatPhones.py
+++ scripts/calculateBestWhatPhones.py
@@ -63,113 +63,168 @@ def getPhoneInfo(phoneID):
 
 	return phone
 
+def getUnSortedPhoneListForKeywordWithFinalRating(keyword, phoneList):
+	allKeywordPhoneList = []
+	finalPhoneList = []
+	maxPosPlusNeg = 0
+	for allPhones in phoneList:
+		allReqPhoneDetails = {}
+		phoneLowest = 0
+		phoneName = allPhones['Model Name']
+		Pricefact = 0
+		
+		phonePrice = mongo.prices.find({'Model Name':phoneName})
+		for prices in phonePrice:
+			realP = prices['ECommercePrice']
+			if realP > 0:
+				if prices['ECommerceStatus'] == 1:
+					if phoneLowest < realP:
+						phoneLowest = realP
+						if phoneLowest < 10000:
+							Pricefact = 1
+						elif phoneLowest < 20000  and phoneLowest >= 10000:
+							Pricefact = 1.05
+						elif phoneLowest < 30000  and phoneLowest >= 20000:
+							Pricefact = 1.1
+						elif phoneLowest < 40000  and phoneLowest >= 30000:
+							Pricefact = 1.15
+						elif phoneLowest < 50000  and phoneLowest >= 40000:
+							Pricefact = 1.2
+						elif phoneLowest < 60000  and phoneLowest >= 50000:
+							Pricefact = 1.2
+						else:
+							Pricefact = 1.2
+		allReqPhoneDetails['ECommercePrice'] = phoneLowest
+		if phoneLowest >0:
+			PosPlusNeg = allPhones['Negative'] + allPhones['Positive']
+
+			if maxPosPlusNeg < PosPlusNeg:
+				maxPosPlusNeg = PosPlusNeg
+			allReqPhoneDetails['TotalRating'] = PosPlusNeg
+			allReqPhoneDetails['ECommercePrice'] = phoneLowest
+			allReqPhoneDetails['Model Name'] = phoneName
+			allReqPhoneDetails['Rating'] = allPhones['Rating']
+			allReqPhoneDetails['Pricefact'] = Pricefact
+			allReqPhoneDetails['Negative'] = allPhones['Negative']
+			allReqPhoneDetails['Positive'] = allPhones['Positive']
+			allKeywordPhoneList.append(allReqPhoneDetails)
+			
+	maxfinalRat = 0
+	maxfinalRatName = 0
+	for allPhones in allKeywordPhoneList:
+		finalRating = 0
+		
+		finalRating = allPhones['finalRating'] = allPhones['Rating']*(1+1.7*(allPhones['Negative'] + allPhones['Positive'])/maxPosPlusNeg)*allPhones["Pricefact"]*10
+		if finalRating > 100:
+			finalRating = 100
+		if maxfinalRat < finalRating:
+			maxfinalRat = finalRating
+			maxfinalRatName = allPhones['ECommercePrice']
+		finalPhoneList.append(allPhones)
+	return finalPhoneList
+
+
+def computeListAndPutToDB(finalPhoneList, keyword):
+	finalPhoneList = sorted(finalPhoneList, key=lambda k: k['finalRating']) 
+	finalPhoneList = finalPhoneList[::-1]
+	print finalPhoneList
+	finalPhoneListWithScore = []
+	tempFinalList = []
+	for allPhones in finalPhoneList:
+		tempPhoneDetail = {}
+		brandName = allPhones['Model Name']
+		brandName = brandName.split(" ")
+		brandName.pop(0)
+		brandName = " ".join(brandName)
+		modelID = ""
+		try:
+			modelID = mongo.phones.find_one({'Model Name': brandName})['_id']
+			tempPhoneDetail = getPhoneInfo(modelID)
+			tempPhoneDetail['finalRating'] = allPhones['finalRating']
+			finalPhoneListWithScore.append(tempPhoneDetail)
+			
+		except:
+			pass
+
+			# print allPhones['Model Name']
+
+	shortList = finalPhoneListWithScore[:15]
+	longList = finalPhoneListWithScore
+	bestCollection = mongo.bestCollection
+
+	bestKeyWordPhones = mongo.bestCollection.find({'keyword':keyword})
+	for allPhones in bestKeyWordPhones:
+		mongo.bestCollection.remove(allPhones['_id'])
+
+	bestCollection.insert({
+			'keyword' : keyword,
+			"short" : shortList,
+			"long" : longList
+		})
+
+	bestKeyWordPhones = mongo.bestCollection.find({'keyword':keyword})
+	for allPhones in bestKeyWordPhones:
+		print allPhones['_id']
+	print "Best" , keyword , "Uploaded"
+
+	
 
 argList = []
 argList =  sys.argv
 argList.pop(0)
 keyword = argList.pop(0)
-phoneList = mongo.keywords.find({'Keyword':keyword})
-allKeywordPhoneList = []
-finalPhoneList = []
-maxPosPlusNeg = 0
-for allPhones in phoneList:
-	allReqPhoneDetails = {}
-	phoneLowest = 0
-	phoneName = allPhones['Model Name']
-	Pricefact = 0
-	
-	phonePrice = mongo.prices.find({'Model Name':phoneName})
-	for prices in phonePrice:
-		realP = prices['ECommercePrice']
-		if realP > 0:
-			if prices['ECommerceStatus'] == 1:
-				if phoneLowest < realP:
-					phoneLowest = realP
-					if phoneLowest < 10000:
-						Pricefact = 1
-					elif phoneLowest < 20000  and phoneLowest >= 10000:
-						Pricefact = 1.15
-					elif phoneLowest < 30000  and phoneLowest >= 20000:
-						Pricefact = 1.3
-					elif phoneLowest < 40000  and phoneLowest >= 30000:
-						Pricefact = 1.45
-					elif phoneLowest < 50000  and phoneLowest >= 40000:
-						Pricefact = 1.6
-					elif phoneLowest < 60000  and phoneLowest >= 50000:
-						Pricefact = 1.6
-					else:
-						Pricefact = 1.6
-	allReqPhoneDetails['ECommercePrice'] = phoneLowest
-	if phoneLowest >0:
-		PosPlusNeg = allPhones['Negative'] + allPhones['Positive']
+#For All Best Reco
+if keyword == "All":
+	bestCollection = mongo.bestCollection
+	bestKeyword = {'Camera':.25, 'Battery':.3, 'Low heating': .1,'Design and build quality': .2, 'Screen': .15}
+	allPhones = []
+	for phone in mongo.phones.find():
+		allPhone = {}
+		try:
+			allPhone["_id"] = phone["_id"]
+			allPhone["Model Name"] = phone["Brand"] + " " + phone["Model Name"]
+			allPhone["Brand"] = phone["Brand"]
+			allPhones.append(allPhone)
+		except:
+			pass
+	allPhoneWithKeyWordRating = []
+	allPhoneWithAllKeyWordRating = []
+	keyPhoneDictWithFinalRating = []
+	for keyword in bestKeyword:
+		KeyWordSpecificList = []
+		for phone in allPhones:
+			keywordForPhone = mongo.keywords.find_one({'Model Name': phone['Model Name'], 'Keyword':keyword})
+			if keywordForPhone == None:
+				keywordForPhone = {'Rating': 0, 'Keyword': 'Battery', 'Positive': 0, 'Brand': phone["Brand"], 'Negative': 0, 'Neutral': 0, 'Model Name': phone['Model Name'], 'finaRating':0.0}
+			KeyWordSpecificList.append(keywordForPhone)
+		allPhoneWithKeyWordList =  getUnSortedPhoneListForKeywordWithFinalRating(keyword,KeyWordSpecificList)
+		for iter in range(len(allPhoneWithKeyWordList)):
+			try:
+				localList = keyPhoneDictWithFinalRating[iter]
+			except:
+				localList = ['',0.0]
+				keyPhoneDictWithFinalRating.append(localList)
 
-		if maxPosPlusNeg < PosPlusNeg:
-			maxPosPlusNeg = PosPlusNeg
-		allReqPhoneDetails['TotalRating'] = PosPlusNeg
-		allReqPhoneDetails['ECommercePrice'] = phoneLowest
-		allReqPhoneDetails['Model Name'] = phoneName
-		allReqPhoneDetails['Rating'] = allPhones['Rating']
-		allReqPhoneDetails['Pricefact'] = Pricefact
-		allReqPhoneDetails['Negative'] = allPhones['Negative']
-		allReqPhoneDetails['Positive'] = allPhones['Positive']
-		allKeywordPhoneList.append(allReqPhoneDetails)
-		
-maxfinalRat = 0
-maxfinalRatName = 0
-for allPhones in allKeywordPhoneList:
-	finalRating = 0
-	
-	finalRating = allPhones['finalRating'] = allPhones['Rating']*(1+(allPhones['Negative'] + allPhones['Positive'])/maxPosPlusNeg)*allPhones["Pricefact"]*10
-	if finalRating > 100:
-		finalRating = 100
-	if maxfinalRat < finalRating:
-		maxfinalRat = finalRating
-		maxfinalRatName = allPhones['ECommercePrice']
-	finalPhoneList.append(allPhones)
-	# print allPhones['TotalRating'], "," ,allPhones['ECommercePrice'], "," ,allPhones['Model Name'], "," ,allPhones['Rating'],"," , allPhones['Pricefact'], "," ,allPhones['Negative'], "," ,allPhones['Positive'] ,"," , finalRating
+			modelName = allPhoneWithKeyWordList[iter]['Model Name']
+			finaRating = allPhoneWithKeyWordList[iter]['finalRating']
+			localList = [modelName,localList[1]+finaRating*bestKeyword[keyword]]
+			keyPhoneDictWithFinalRating[iter] = localList
+	finalPhoneList = []
+	for phones in keyPhoneDictWithFinalRating:
+		phoneDict = {}
+		phoneDict['Model Name'] = phones[0]
+		phoneDict['finalRating'] = phones[1]
+		finalPhoneList.append(phoneDict)
+	computeListAndPutToDB(finalPhoneList, 'All')
 
-finalPhoneList = sorted(finalPhoneList, key=lambda k: k['finalRating']) 
-finalPhoneList = finalPhoneList[::-1]
-finalPhoneListWithScore = []
-tempFinalList = []
-for allPhones in finalPhoneList:
-	tempPhoneDetail = {}
-	brandName = allPhones['Model Name']
-	brandName = brandName.split(" ")
-	brandName.pop(0)
-	brandName = " ".join(brandName)
-	modelID = ""
-	try:
-		modelID = mongo.phones.find_one({'Model Name': brandName})['_id']
-		tempPhoneDetail = getPhoneInfo(modelID)
-		tempPhoneDetail['finalRating'] = allPhones['finalRating']
-		finalPhoneListWithScore.append(tempPhoneDetail)
-		
-	except:
-		pass
+else:
+	#For Keyword Specific
+	phoneList = mongo.keywords.find({'Keyword':keyword})
+	finalPhoneList = []
+	finalPhoneList = getUnSortedPhoneListForKeywordWithFinalRating(keyword, phoneList)
+	computeListAndPutToDB(finalPhoneList, keyword)
+		# print allPhones['TotalRating'], "," ,allPhones['ECommercePrice'], "," ,allPhones['Model Name'], "," ,allPhones['Rating'],"," , allPhones['Pricefact'], "," ,allPhones['Negative'], "," ,allPhones['Positive'] ,"," , finalRating
 
-		# print allPhones['Model Name']
-
-shortList = finalPhoneListWithScore[:15]
-longList = finalPhoneListWithScore
-bestCollection = mongo.bestCollection
-
-bestKeyWordPhones = mongo.bestCollection.find({'keyword':keyword})
-for allPhones in bestKeyWordPhones:
-	mongo.bestCollection.remove(allPhones['_id'])
-
-bestCollection.insert({
-		'keyword' : keyword,
-		"short" : shortList,
-		"long" : longList
-	})
-
-bestKeyWordPhones = mongo.bestCollection.find({'keyword':keyword})
-for allPhones in bestKeyWordPhones:
-	print allPhones['_id']
-
-
-print "Best" , keyword , "Uploaded"
 
 
 
